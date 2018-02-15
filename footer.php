2fd0b4be454fc9f2b3e9310380234aace4afda33
RobertoDO <33458793+RobertoDO@users.noreply.github.com>
2018-02-15 11:15:40 -0500

    ...
    ...

--- footer.php
+++ footer.php
@@ -1,15 +1,10 @@
-    
-    <footer class="footer" >
-    	<hr>
-      <div class="container" >		
-      
-        <H6 class="text-muted text-center"  >Todos los Derechos reservados ©CeosTours 2018. Desarrollado por Jengibre.</H6>
-        
-      </div>
 
-      
-    	
+    <footer class="footer">
+      <div class="container">
+        <span class="text-muted">Derechos reservados &copy; Ceos Tour 2018.</span>
+      </div>
     </footer>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
