ac218696e58e288696db9f2f3bdb732d8b9eff93
Maybol-Reynaldo <maybol.huracahua@tecsup.edu.pe>
2018-02-15 11:16:56 -0500

    origilnal
    original

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Ceos</title>
+    <title>Ceoss</title>
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/jquery.lettering.js"></script>
     <script src="js/jquery.fittext.js"></script>
