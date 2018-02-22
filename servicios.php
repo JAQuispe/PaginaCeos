24068efe3c34e3d807cd7c595a4e305691679fe1
Maybol-Reynaldo <maybol.huracahua@tecsup.edu.pe>
2018-02-22 14:28:56 -0500

    servicios
    servicios img

--- servicios.php
+++ servicios.php
@@ -8,24 +8,17 @@
       <section class="ml-5 mr-5">
         <hr>
         <div class ="row">
-          <div class="col-md-9">
+          <div class="col-md-8">
             <h3>Convenios empresariales</h3>
-            <p>
-              Es un  servicio para  empresas que buscan un taxi seguro.s
-              La facturación debe ser superior a S/. 2,000.00 nuevos soles mensuales.
-              ¿Cómo funciona?
-              Taxitel proporciona talonario de Órdenes de servicios (Vales)
-              El usuario efectúa el pago por los servicios de taxi con VALES.
-              El talonario de vales consta de una original y 2 copias, los
-              cuales se distribuyen para ambas partes.
+            <p >
 
-              Condiciones de pago:
-              La factura se realiza con corte al último día hábil del mes,  y
-              se emite los diez  primeros días del siguiente mes, a un plazo
-              máximo de 30 días.
+              Es un  servicio para  empresas que buscan un taxi seguros.
+              La facturación debe ser superior a S/. 2,000.00 nuevos soles mensuales.
+             
             </p>
           </div>
-          <div class="col-md-3">
+          <div class="col-md-1"></div>
+          <div class="col-md-3 ">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/aaa.png" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
           </div>
         </div>
@@ -36,15 +29,15 @@
           <div class="col-md-9" style="text-align: right">
             <h4> <center>Courier</center>
             </h4>
-            <p><center>Entrega y recojo de paquetes o documentos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, repellendus nulla modi itaque error similique, odit dolorem, consectetur accusantium delectus magni qui harum! Delectus, sed esse. Placeat quasi, vero sequi.</center></p>
+            <p>Entrega y recojo de paquetes o documentos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, repellendus nulla modi itaque error similique, odit dolorem, consectetur accusantium delectus magni qui harum! Delectus, sed esse. Placeat quasi, vero sequi.</p>
           </div>
           
         </div>
 
         <div class="row">
-          <div class="col-md-9">
-            <h4>Servicios al aeropuerto</h4>
-            <p>Traslados y recojo al aeropuerto a bajos precios</p>
+          <div class="col-md-9" >
+            <h4><center>Servicios al aeropuerto</center></h4>
+            <p>Traslados y recojo al aeropuerto a bajos precios Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, porro aperiam quae incidunt eum numquam repellendus autem. Magnam, quo neque suscipit quia. Perspiciatis tempora totam, fugit minus nihil delectus quidem.</p>
           </div>
           <div class="col-md-3">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/bbb.png" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
@@ -53,39 +46,41 @@
 
       
 
-        <div class="row">
-          <div class="col-md-9">
-            <h4>City Tour</h4>
+        <div class="row" >
+        <div class="col-md-3 ">
+            <img class="rounded-circle hvr-bounce-in" src="imagenes/turismo.JPG" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
+          </div>
+          <div class="col-md-9 " style="text-align: right">
+            <h4><center >City Tour </center></h4>
             <p>
-              Iglesia de la compañía, Mirador de Yanahuara, Monasterio de Santa
+             Iglesia de la compañía, Mirador de Yanahuara, Monasterio de Santa
               Catalina, Plaza de Armas, Mirador de Sachaca, Mansión del fundador,
               etc.
               También realizamos Tour a Cañón del Colca.
             </p>
           </div>
-          <div class="col-md-3 ">
-            <img class="rounded-circle hvr-bounce-in" src="imagenes/turismo.JPG" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
-          </div>
+          
         </div>
 
         <div class="row">
           <div class="col-md-9">
-            <h4>Matrimonios</h4>
-            <p>Arreglo floral en la unidad y otros detalles a solicitud con anticipación.</p>
+            <h4><center>Matrimonios  </center>  </h4>
+            <p><center>Arreglo floral en la unidad y otros detalles a solicitud con anticipación.</center></p>
           </div>
           <div class="col-md-3">
-            <img class="rounded-circle hvr-bounce-in" src="imagenes/ccc.png" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
+            <img class="rounded-circle hvr-bounce-in" src="imagenes/matrimonio.JPG" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
           </div>
         </div>
 
         <div class="row">
-          <div class="col-md-9">
-            <h4>Servicio Especial</h4>
+        <div class="col-md-3">
+            <img class="rounded-circle hvr-bounce-in" src="imagenes/serviciosespeciales.jpg" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
+          </div>
+          <div class="col-md-9" style="text-align: right">
+            <h4><center> Servicio Especial </center></h4>
             <p>Traslados a conos, playas, centros campestres, etc.</p>
           </div>
-          <div class="col-md-3">
-            <img class="rounded-circle hvr-bounce-in" src="imagenes/ccc.png" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
-          </div>
+          
         </div>
       </section>
       <br>
