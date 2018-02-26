5d5229bd267cc1f228d161f44958a345f05961df
RobertoDO <33458793+RobertoDO@users.noreply.github.com>
2018-02-26 11:22:58 -0500

    Color header
    ...

--- servicios.php
+++ servicios.php
@@ -10,11 +10,9 @@
         <div class ="row">
           <div class="col-md-8">
             <h3>Convenios empresariales</h3>
-            <p >
-
+            <p>
               Es un  servicio para  empresas que buscan un taxi seguros.
               La facturación debe ser superior a S/. 2,000.00 nuevos soles mensuales.
-             
             </p>
           </div>
           <div class="col-md-1"></div>
@@ -22,44 +20,50 @@
             <img class="rounded-circle hvr-bounce-in" src="imagenes/aaa.png" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
           </div>
         </div>
-          <div class="row" >
-        <div class="col-md-3">
+
+        <div class="row" >
+          <div class="col-md-3">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/courier.JPG" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
           </div>
           <div class="col-md-9" style="text-align: right">
-            <h4> <center>Courier</center>
-            </h4>
-            <p>Entrega y recojo de paquetes o documentos. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, repellendus nulla modi itaque error similique, odit dolorem, consectetur accusantium delectus magni qui harum! Delectus, sed esse. Placeat quasi, vero sequi.</p>
+            <h4><center>Courier</center></h4>
+            <p>
+              Entrega y recojo de paquetes o documentos. Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Optio, repellendus nulla modi itaque error
+              similique, odit dolorem, consectetur accusantium delectus magni qui harum!
+              Delectus, sed esse. Placeat quasi, vero sequi.
+            </p>
           </div>
-          
         </div>
 
         <div class="row">
           <div class="col-md-9" >
             <h4><center>Servicios al aeropuerto</center></h4>
-            <p>Traslados y recojo al aeropuerto a bajos precios Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, porro aperiam quae incidunt eum numquam repellendus autem. Magnam, quo neque suscipit quia. Perspiciatis tempora totam, fugit minus nihil delectus quidem.</p>
+            <p>
+              Traslados y recojo al aeropuerto a bajos precios Lorem ipsum dolor sit
+              amet, consectetur adipisicing elit. Recusandae, porro aperiam quae incidunt
+              eum numquam repellendus autem. Magnam, quo neque suscipit quia. Perspiciatis
+              tempora totam, fugit minus nihil delectus quidem.
+            </p>
           </div>
           <div class="col-md-3">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/bbb.png" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
           </div>
         </div>
 
-      
-
         <div class="row" >
-        <div class="col-md-3 ">
+          <div class="col-md-3 ">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/turismo.JPG" height="200px" width="200px">  <!-- la imagen debe estar 200 x 200 px-->
           </div>
           <div class="col-md-9 " style="text-align: right">
             <h4><center >City Tour </center></h4>
             <p>
-             Iglesia de la compañía, Mirador de Yanahuara, Monasterio de Santa
+              Iglesia de la compañía, Mirador de Yanahuara, Monasterio de Santa
               Catalina, Plaza de Armas, Mirador de Sachaca, Mansión del fundador,
               etc.
               También realizamos Tour a Cañón del Colca.
             </p>
           </div>
-          
         </div>
 
         <div class="row">
@@ -73,14 +77,13 @@
         </div>
 
         <div class="row">
-        <div class="col-md-3">
+          <div class="col-md-3">
             <img class="rounded-circle hvr-bounce-in" src="imagenes/serviciosespeciales.jpg" height="200px" width="200px" >  <!-- la imagen debe estar 200 x 200 px-->
           </div>
           <div class="col-md-9" style="text-align: right">
             <h4><center> Servicio Especial </center></h4>
             <p>Traslados a conos, playas, centros campestres, etc.</p>
           </div>
-          
         </div>
       </section>
       <br>
